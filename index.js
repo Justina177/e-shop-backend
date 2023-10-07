8ca5de7c6272fdffb18119caf13f35631234f92e
justina177 <justinaenenche@yahoo.com>
2023-10-07 22:08:45 +0100

    Added database connection

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const dotenv = ("dotenv").config();
-const PORT = process.env.PORT || 4000;
-
 const app = express();
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 4000;
+dotenv.config();
+
+const connect = require('./config/connect.js');
+
+connect(); 
+app.use('/', (req, res) => {
+    res.send("Hello from Server side")
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server is Running at PORT ${PORT}`);
