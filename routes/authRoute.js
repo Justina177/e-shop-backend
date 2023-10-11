0a429f820a3ca3184fcb5b474b1d6a516f61309e
justina177 <justinaenenche@yahoo.com>
2023-10-11 09:00:11 +0100

    Added function to verify is user is an admin

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { createUser, loginUser, getAllUsers, getUser, deleteUser, updateUser } = require("../controller/userController.js");
-const authMiddleware = require("../middlewares/authMiddleware.js");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware.js");
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/all-users", getAllUsers);
-router.get("/:id",authMiddleware, getUser)
+router.get("/:id", authMiddleware, isAdmin, getUser)
 router.delete("/:id", deleteUser);
 router.put("/:id", updateUser)
 
