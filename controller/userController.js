a665ebd2f24c4d145de0d208a0848dbe3ffeecd5
justina177 <justinaenenche@yahoo.com>
2023-10-14 07:18:45 +0100

    Added product model

--- controller/userController.js
+++ controller/userController.js
@@ -53,7 +53,6 @@ const loginUser = asyncHandler(async(req, res) => {
 // Handle Refresh Token
 const handleRefreshToken = asyncHandler(async (req, res) => {
     const cookie = req.cookies;
-    console.log(cookie);  
     if (!cookie?.refreshToken) throw new Error("No Refresh Token in Cookies");
     const refreshToken = cookie.refreshToken;
     console.log(refreshToken);
