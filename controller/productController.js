7bfe2a5048a116abd78708ff75ada43a1094c345
justina177 <justinaenenche@yahoo.com>
2023-10-15 03:15:51 +0100

    Installed slugify to handle slug

--- controller/productController.js
+++ controller/productController.js
@@ -1,13 +1,16 @@
 const Product = require("../models/productModule");
 const asyncHandler = require("express-async-handler");
-
+const slugify = require("slugify");
 
 const createProduct = asyncHandler(async (req, res) => {
     try {
+      if (req.body.title) {
+        req.body.slug = slugify(req.body.title);
+      }
         const newProduct = await Product.create(req.body);
         res.json(newProduct);
       } catch (error) {
-        throw new Error(error);
+        throw new Error(error);  
       }
 });
 
